2f4132b58b483ba82c80d0712803b7ebc9b998a6
aldyn <aldin.colic@gmail.com>
2014-12-15 20:13:26 +0100

    Update MySorter.java

--- zavrseno/zadatak1/MySorter.java
+++ zavrseno/zadatak1/MySorter.java
@@ -4,13 +4,12 @@ import java.util.Collections;
 public class MySorter{
 	
  public static void sortByBirthday(List<Person> p){
-
-	 
 	 Collections.sort(p);
 	 for(int i=0; i < p.size(); i++)
-		 System.out.println(i+1 +". " + p.get(i).toString());
-	 
-	 
+		 System.out.println(i+1 +". " + p.get(i));
+/*Ne mora se explicitno pozivati toString() metod jer metod println kreira novi Object
+ * na osnovu proslijedjene vrijendosti, tj. poziva metod valueOf() koji explicitno
+ * na proslijedjenom parametru pozove metod toString();	 
+ */
  }
-
 };

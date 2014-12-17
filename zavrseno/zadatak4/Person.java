5f38c82609988522075d09f94023f726c89b59b6
aldyn <aldin.colic@gmail.com>
2014-12-17 19:51:44 +0100

    Update Person.java

--- zavrseno/zadatak4/Person.java
+++ zavrseno/zadatak4/Person.java
@@ -8,26 +8,25 @@ public class Person {
 	public static Person getInstance(){
 		return new Person();
 	}
-	public Person withFirstName(String first_name){
-		firstName = first_name;
+	public Person withFirstName(String firstName){
+		this.firstName = firstName;
 		return this;
 	}
-	public Person withLastName(String last_name){
-		lastName=last_name;
+	public Person withLastName(String lastName){
+		this.lastName=lastName;
 		return this;
 	}
-	public Person bornOn(String date){
-		birthday=date;
+	public Person bornOn(String birthday){
+		this.birthday=birthday;
 		return this;
 	}
 	@Override
 	public String toString(){
-		
 		return firstName + " " + lastName + " " + birthday;
 	}
 	
 	public static void main(String argv[]){
 		Person p = Person.getInstance().withFirstName("Adnan").withLastName("Alicic").bornOn("19.03.1987");
-		System.out.println(p.toString());
+		System.out.println(p);
 	}
 }

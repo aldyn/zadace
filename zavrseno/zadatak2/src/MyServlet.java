05b9f7403a097d1e642f7e5abd52c482b99436c1
aldyn <aldin.colic@gmail.com>
2014-12-17 19:40:48 +0100

    Update and rename myServlet.java to MyServlet.java

--- zavrseno/zadatak2/src/MyServlet.java
+++ zavrseno/zadatak2/src/MyServlet.java
@@ -11,61 +11,44 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.Session;
 
-import zadatak2.userMessage;
+import zadatak2.UserMessage;
 
 import java.util.List;
 import java.util.LinkedList;
 
-/**
- * Servlet implementation class myServlet
- */
-@WebServlet("/myServlet")
-public class myServlet extends HttpServlet {
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public myServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//chatRoom.jsp
 		ServletContext sc = getServletContext();
 		
 		if(request.getParameter("submit") != null){
 				request.getSession().setAttribute("username", request.getParameter("user"));
-				response.sendRedirect("chatRoom.jsp?username="+request.getSession().getAttribute("username"));
-			}		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub		
+				response.sendRedirect("ChatRoom.jsp?username="+request.getSession().getAttribute("username"));
+		}		
+		else{
+			List<UserMessage> messages = (List)sc.getAttribute("messages");
+			for(int i=0; i<messages.size(); i++){
+				request.setAttribute("message", messages.get(i));
+			}
+		}
+	}	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		ServletContext sc = getServletContext();
-		List<userMessage> messages = (LinkedList)sc.getAttribute("messages");
+		List<UserMessage> messages = (List)sc.getAttribute("messages");
 		String user=(String)request.getSession().getAttribute("username");		
 		if(request.getParameter("send") != null){			
 			String message =(String) request.getParameter("message");						
 			if(messages != null){
-				messages.add(new userMessage(user, message));
+				messages.add(new UserMessage(user, message));
 			}
 			else{
 				messages = new LinkedList();
-				messages.add(new userMessage(user, message));
+				messages.add(new UserMessage(user, message));
 				sc.setAttribute("messages", messages);
 			}
 		}
-		response.sendRedirect("chatRoom.jsp?username="+user);		
-
+		response.sendRedirect("ChatRoom.jsp?username="+user);		
 	}
-
 }

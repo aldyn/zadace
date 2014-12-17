aeaad12ffada8c79a8f5be0a1ad9a30808ef141d
aldyn <aldin.colic@gmail.com>
2014-12-17 19:42:10 +0100

    Update and rename userMessage.java to UserMessage.java

---
+++ zavrseno/zadatak2/src/zadatak2/UserMessage.java
@@ -0,0 +1,17 @@
+package zadatak2;
+
+public class UserMessage{
+	private String user;
+	private String message;
+	
+	public UserMessage(String user, String message){
+		this.user = user;
+		this.message = message;
+	}
+	public String getUser(){
+		return user;
+	}
+	public String getMessage(){
+		return message;
+	}	
+};	
